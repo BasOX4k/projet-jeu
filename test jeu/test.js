04f772b4782be4a78bbc084f9b01e4a3178b779d
Timothée <adodo.timotheec@gmail.com>
2024-02-06 16:16:36 +0100

    animation joueur 2

--- test jeu/test.js
+++ test jeu/test.js
@@ -1,6 +1,32 @@
 const sprite = document.getElementById('mickey');
 const step = 10; // Nombre de pixels par pas de déplacement
 
+
+const spriteHammer = document.getElementById('hammer');
+const stepHammer = 10;
+
+window.addEventListener('keydown', moveHammer);
+function moveHammer(event) {
+  const keyPressedHammer = event.key;
+  if (keyPressedHammer === 'é') {
+    moveRightH();
+  }
+  if (keyPressedHammer === '&') {
+    moveLeftH();
+  }
+}
+function moveRightH() {
+  const currentPosition = parseInt(hammer.style.left) || 0;
+  const newPosition = currentPosition + step;
+  hammer.style.left = `${newPosition}px`;
+}
+
+function moveLeftH() {
+  const currentPosition = parseInt(hammer.style.left) || 0;
+  const newPosition = currentPosition - step;
+  hammer.style.left = `${newPosition}px`;
+}
+
 window.addEventListener('keydown', moveSprite);
 
 function moveSprite(event) {
@@ -9,9 +35,10 @@ function moveSprite(event) {
   if (keyPressed === 'ArrowRight') {
     moveRight();
   } 
-  if (keyPressed === 'ArrowLeft'){ 
+  if (keyPressed === 'ArrowLeft') { 
     moveLeft();
   }
+ 
 }
 
 function moveRight() {
@@ -20,7 +47,20 @@ function moveRight() {
   mickey.style.left = `${newPosition}px`;
 }
 function moveLeft() {
-  const currentPosition = parseInt(mickey.style.right) ||0;
-  const newPosition = currentPosition + step;
-  mickey.style.right = `${newPosition}px`;
+  const currentPosition = parseInt(mickey.style.left) || 0;
+  const newPosition = currentPosition - step;
+  mickey.style.left = `${newPosition}px`;
 }
+
+
+// document.addEventListener('keydown', jump);
+
+// function jump(event) {
+  // if (event.keyCode === 32) { // Touche "Espace"
+    // const sprite = document.getElementById('sprite');
+    // sprite.style.animation = 'jump 0.5s ease alternate'; // Lancer l'animation de saut
+    // setTimeout(() => { // Réinitialiser l'animation après le saut
+      // sprite.style.animation = '';
+    // }, 500);
+  // }
+// }
